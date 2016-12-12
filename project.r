6addd5645d4205fa3575f24549c381680d8abf4c
cd10012 <ced361@nyu.edu>
2016-12-11 20:43:50 -0500

    Sanitize the dataset
    removed all unknown characters / emoji’s from the tweets, also
    converted the tweets to lowercase

--- project.r
+++ project.r
@@ -21,16 +21,19 @@ setup_twitter_oauth(key, secret, access_token, access_secret)
 twLJ = searchTwitter('Los Angeles Lakers', n = 3000, since = '2016-12-05', until = '2016-12-06')
 twLM = searchTwitter('Los Angeles Lakers', n = 3000, since = '2016-12-03', until = '2016-12-04')
 twLB = searchTwitter('Los Angeles Lakers', n = 3000, since = '2016-11-30', until = '2016-12-01')
+twLS = searchTwitter('Los Angeles Lakers', n = 3000, since = '2016-12-09', until = '2016-12-10')
 
 #save a copy as backup
 saveRDS(twLJ, './NBA_TweetsLakersJazz.RDS')
 saveRDS(twLM, './NBA_TweetsLakersMemphis.RDS')
 saveRDS(twLB, './NBA_TweetsLakersBulls.RDS')
+saveRDS(twLS, './NBA_TweetsLakersBulls.RDS')
 
 #loading from file 
 twLJ = readRDS('./NBA_TweetsLakersJazz.RDS')
 twLM = readRDS('./NBA_TweetsLakersJazz.RDS')
 twLB = readRDS('./NBA_TweetsLakersBulls.RDS')
+twLS = readRDS('./NBA_TweetsLakersBulls.RDS')
 
 #Convert to our timezone and refine the filter to game start and end time 
 #LakersVJazz Starttime: 7:30pm
@@ -41,13 +44,33 @@ LiveTweetsLakersJazz<- subset(LakersJazz, format(LakersJazz$created, "%H:%M:$S")
 
 #LakersVMemphisGrizzly's Starttime: 8:00pm
 LakersMemphis = twListToDF(twLM)
+options(tz="America/New York")
 LakersMemphis$created <- with_tz(LakersMemphis$created, 'America/New York')
 LiveTweetsLakersMemphis<- subset(LakersMemphis, format(LakersMemphis$created, "%H:%M:$S") > "20:00:00" & format(LakersMemphis$created, "%H:%M:$S") < "23:00:00")
 
 #LakersVChicagoBulls Starttime: 8:00pm
 LakersBulls = twListToDF(twLB)
+options(tz="America/New York")
 LakersBulls$created <- with_tz(LakersBulls$created, 'America/New York')
 LiveTweetsLakersBulls <- subset(LakersBulls, format(LakersBulls$created, "%H:%M:$S") > "20:00:00" & format(LakersBulls$created, "%H:%M:$S") < "23:00:00")
+
+#LakersVPheonixSuns Starttime: 10:30pm
+LakersSuns = twListToDF(twLS)
+options(tz="America/New York")
+LakersSuns$created <- with_tz(LakersSuns$created, 'America/New York')
+LiveTweetsLakersSuns <- subset(LakersSuns, format(LakersSuns$created, "%H:%M:$S") > "21:30:00" & format(LakersSuns$created, "%H:%M:$S") < "24:00:00")
+
+#Remove emoji's and other unknown characters from the dataset
+LiveTweetsLakersSuns$text <- sapply(LiveTweetsLakersSuns$text,function(row) iconv(row, "latin1", "ASCII", sub=""))
+LiveTweetsLakersBulls$text <- sapply(LiveTweetsLakersSuns$text,function(row) iconv(row, "latin1", "ASCII", sub=""))
+LiveTweetsLakersMemphis$text <- sapply(LiveTweetsLakersSuns$text,function(row) iconv(row, "latin1", "ASCII", sub=""))
+LiveTweetsLakersJazz$text <- sapply(LiveTweetsLakersSuns$text,function(row) iconv(row, "latin1", "ASCII", sub=""))
+
+# Make all the tweets lower case to make n-grams analysis easier
+LiveTweetsLakersSuns$text <-sapply(LiveTweetsLakersSuns$text, tolower)
+LiveTweetsLakersBulls$text <-sapply(LiveTweetsLakersSuns$text, tolower)
+LiveTweetsLakersMemphis$text <-sapply(LiveTweetsLakersSuns$text, tolower)
+LiveTweetsLakersJazz$text <-sapply(LiveTweetsLakersSuns$text, tolower)
 
 #setting up corpus
 basketballTerms= read.table("Basketball corpus.txt")
